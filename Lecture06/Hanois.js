1c71f87dc9e33b150dfcaefa4a801817a85eb403
idioteque <wklee316@naver.com>
2022-04-13 15:44:35 +0900

    hanoi_06

---
+++ Lecture06/Hanois.js
@@ -0,0 +1,14 @@
+function init()
+{
+    var arr = [];
+
+    arr.push("원소은");
+    arr.push("표승현");
+    arr.push("최민주");
+    arr.push("전가은");
+    arr.push("이현진");
+    arr.push("최현우");
+
+    console.log(arr.pop());
+    console.log(arr.shift());
+}
